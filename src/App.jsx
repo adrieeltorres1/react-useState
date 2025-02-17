6768b7114eabbb624d457f2a87da0e2a4473aaed
adrieeltorres1 <adrieltorreslds@gmail.com>
2025-02-17 13:55:50 -0300

    Finalizando a estilização da interface do form

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,52 @@
 const App = () => {
+  const [aluno, setAluno] = ('')
+  const [genero, setGenero] = ('')
+  const [idade, setIdade] = ('')
+  const [alunos, setAlunos] = []
+
+
+
   return (  
     <>
-      <form className="flex justify-center m-8 border-2 border-blue-300 w-[300px] h-[350px]">
+      <form className="flex justify-center 
+        m-8 rounded-lg 
+        border-2 
+        bg-slate-100 
+        border-blue-200 
+        w-[300px] 
+        h-[350px]">
           <div className="flex flex-col mt-4">
+
             <label htmlFor="aluno">Nome do aluno(a)</label>
-            <input className="rounded-md  border-2 border-gray-200 w-[250px] p-3" type="text" id="aluno"/>
+            <input className="rounded-md  
+              border-2 
+              border-gray-200 
+              w-[250px] 
+              p-3" 
+              type="text" 
+              id="aluno" 
+              placeholder="Digite o nome do aluno" required/>
 
             <label htmlFor="genero">Gênero</label>
-            <input className="rounded-md border-2 border-gray-200 w-[250px] p-3" type="text" id="genero"/>
+            <input className="rounded-md 
+              border-2 
+              border-gray-200 
+              w-[250px] 
+              p-3" 
+              type="text" 
+              id="genero" 
+              placeholder="Digite o gênero" required/>
 
             <label htmlFor="idade">Idade</label>
-            <input className="rounded-md  border-2 border-gray-200 w-[250px] p-3 " type="number" id="idade"/>
+            <input 
+              className="rounded-md  
+              border-2 
+              border-gray-200 
+              w-[250px] 
+              p-3 " 
+              type="number" 
+              id="idade" 
+              placeholder="Digite a idade" required/>
 
             <button className="border-1
               rounded-full
